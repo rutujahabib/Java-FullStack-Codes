7e41cb6ce579a703635653eef72be85d4f6ad6a3
Rutuja Habib <104141040+rutujahabib@users.noreply.github.com>
2026-03-31 21:48:40 +0530

    Add M1 class with main and test methods

---
+++ Methods/M1.java
@@ -0,0 +1,12 @@
+class M1{
+    public static void main(String[] args){
+        System.out.println("From main()");
+        test();
+    }
+    public static void test(){
+        System.out.println("Good Morning!");
+    }
+}
+
+// From main()
+// Good Morning!
