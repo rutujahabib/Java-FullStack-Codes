ae634158d303d80ff3d118bf7ecf84d1740932bc
Rutuja Habib <104141040+rutujahabib@users.noreply.github.com>
2026-04-04 23:53:04 +0530

    Add files via upload

---
+++ Recursion/RecPower.java
@@ -0,0 +1,13 @@
+class RecPower{
+    public static void main(String[] args){
+        System.out.println(power(2,3));   //------> 8
+    } 
+    public static int power(int base, int raise){
+        if(raise==0){
+            return 1;
+        }
+        return base*power(base,raise-1);
+    }
+}
+
+
