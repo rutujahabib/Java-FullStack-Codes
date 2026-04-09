fe692a984f6619c38eba997731c7120062f91685
Rutuja Habib <104141040+rutujahabib@users.noreply.github.com>
2026-04-09 21:30:32 +0530

    Add files via upload

---
+++ Static/StatInit2.java
@@ -0,0 +1,6 @@
+class StatInit2{
+    static double pi = 22.0/7.0;
+    public static void main(String[] args){
+        System.out.println(pi);           ///--------------->3.142857142857143   
+    }
+}
