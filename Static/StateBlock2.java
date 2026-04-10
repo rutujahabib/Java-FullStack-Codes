519f21372e7d2dc0b652bd20f56dca66b2d62a6c
Rutuja Habib <104141040+rutujahabib@users.noreply.github.com>
2026-04-11 00:04:44 +0530

    Add files via upload

---
+++ Static/StateBlock2.java
@@ -0,0 +1,7 @@
+class StateBlock2{
+    public static void main(String[] args){
+        System.out.println(Thread.currentThread().getPriority()); //5
+        System.out.println(Thread.currentThread().getName()); //main
+
+    }
+}
